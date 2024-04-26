9d78e88e46a4fadb31557f1c54e8410b633422e9
M0rEe <62582432+M0rEe@users.noreply.github.com>
2024-04-26 14:38:47 +0300

    Added Functions of Accelerometer

--- Core/Src/MPU6050_program.c
+++ Core/Src/MPU6050_program.c
@@ -10,8 +10,9 @@ extern I2C_HandleTypeDef hi2c1;
 void MPU_6050_Init() {
 	HAL_StatusTypeDef retState = HAL_ERROR;
 //  checking for device availability
-	retState = HAL_I2C_IsDeviceReady(&hi2c1, ((MPU_I2C_ADDRESS << 1) + 0), 1,
-	MPU_MAX_TIMEOUT_MS);
+	retState = HAL_I2C_IsDeviceReady(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_WRITE_OPERATION), 1,
+			MPU_MAX_TIMEOUT_MS);
 #if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
 	if (retState == HAL_OK) {
 		printf("Device is Ready to use\n");
@@ -23,8 +24,9 @@ void MPU_6050_Init() {
 //  Configure Gyroscope
 	uint8_t l_gyroData = (MPU_FS_SEL_500 << MPU_FS_SEL_OFFSET);
 
-	retState = HAL_I2C_Mem_Write(&hi2c1, ((MPU_I2C_ADDRESS << 1) + 0),
-	MPU_GYROSCOPE_CFG_REG, 1, &l_gyroData, 1, MPU_MAX_TIMEOUT_MS);
+	retState = HAL_I2C_Mem_Write(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_WRITE_OPERATION),
+			MPU_GYROSCOPE_CFG_REG, 1, &l_gyroData, 1, MPU_MAX_TIMEOUT_MS);
 #if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
 	if (retState == HAL_OK) {
 		printf("Configured GyroScope\n");
@@ -36,8 +38,9 @@ void MPU_6050_Init() {
 //  Configure Accelerometer
 	uint8_t l_accData = (MPU_AFS_SEL_4g << MPU_AFS_SEL_OFFSET);
 
-	retState = HAL_I2C_Mem_Write(&hi2c1, ((MPU_I2C_ADDRESS << 1) + 0),
-	MPU_ACCELEROMETER_CFG_REG, 1, &l_accData, 1, MPU_MAX_TIMEOUT_MS);
+	retState = HAL_I2C_Mem_Write(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_WRITE_OPERATION),
+			MPU_ACCELEROMETER_CFG_REG, 1, &l_accData, 1, MPU_MAX_TIMEOUT_MS);
 #if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
 	if (retState == HAL_OK) {
 		printf("Configured Accelerometer\n");
@@ -50,8 +53,9 @@ void MPU_6050_Init() {
 //  Configure POWER and Stop SleepMode
 	uint8_t l_pwrData = 0;
 
-	retState = HAL_I2C_Mem_Write(&hi2c1, ((MPU_I2C_ADDRESS << 1) + 0),
-	MPU_PWR_MANAGMENT_REG1, 1, &l_pwrData, 1, MPU_MAX_TIMEOUT_MS);
+	retState = HAL_I2C_Mem_Write(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_WRITE_OPERATION),
+			MPU_PWR_MANAGMENT_REG1, 1, &l_pwrData, 1, MPU_MAX_TIMEOUT_MS);
 #if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
 	if (retState == HAL_OK) {
 		printf("Device Running Mode\n");
@@ -65,7 +69,8 @@ void MPU_6050_Init() {
 uint16_t MPU_6050_getGyroX() {
 	uint8_t l_Data[2];
 	HAL_StatusTypeDef retState = HAL_I2C_Mem_Read(&hi2c1,
-			((MPU_I2C_ADDRESS << 1) + 1), MPU_GYROSCOPE_DATA_REG_START, 1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_READ_OPERATION),
+			MPU_GYROSCOPE_DATA_REG_START + MPU_GYROSCOPE_X_DATA_REG_OFFSET, 1,
 			l_Data, 2, MPU_MAX_TIMEOUT_MS);
 	uint16_t retData = (l_Data[0] << 8) + l_Data[1];
 
@@ -83,7 +88,8 @@ uint16_t MPU_6050_getGyroX() {
 uint16_t MPU_6050_getGyroY() {
 	uint8_t l_Data[2];
 	HAL_StatusTypeDef retState = HAL_I2C_Mem_Read(&hi2c1,
-			((MPU_I2C_ADDRESS << 1) + 1), MPU_GYROSCOPE_DATA_REG_START + 2, 1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_READ_OPERATION),
+			MPU_GYROSCOPE_DATA_REG_START + MPU_GYROSCOPE_Y_DATA_REG_OFFSET, 1,
 			l_Data, 2, MPU_MAX_TIMEOUT_MS);
 	uint16_t retData = (l_Data[0] << 8) + l_Data[1];
 
@@ -101,7 +107,8 @@ uint16_t MPU_6050_getGyroY() {
 uint16_t MPU_6050_getGyroZ() {
 	uint8_t l_Data[2];
 	HAL_StatusTypeDef retState = HAL_I2C_Mem_Read(&hi2c1,
-			((MPU_I2C_ADDRESS << 1) + 1), MPU_GYROSCOPE_DATA_REG_START + 4, 1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_READ_OPERATION),
+			MPU_GYROSCOPE_DATA_REG_START + MPU_GYROSCOPE_Z_DATA_REG_OFFSET, 1,
 			l_Data, 2, MPU_MAX_TIMEOUT_MS);
 	uint16_t retData = (l_Data[0] << 8) + l_Data[1];
 
@@ -119,13 +126,62 @@ uint16_t MPU_6050_getGyroZ() {
 
 uint16_t MPU_6050_getAccX() {
 
+	uint8_t l_Data[2];
+	HAL_StatusTypeDef retState = HAL_I2C_Mem_Read(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_READ_OPERATION),
+			MPU_ACCELEROMETER_DATA_REG_START
+					+ MPU_ACCELEROMETER_X_DATA_REG_OFFSET, 1, l_Data, 2,
+			MPU_MAX_TIMEOUT_MS);
+	uint16_t retData = (l_Data[0] << 8) + l_Data[1];
+
+#if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
+	if (retState == HAL_OK) {
+		printf("X Data retrieved successfully\n");
+	} else {
+		printf("X Data retrieving Failed\n");
+	}
+#else
+	#endif
+	return retData;
 }
 
 uint16_t MPU_6050_getAccY() {
+	uint8_t l_Data[2];
+	HAL_StatusTypeDef retState = HAL_I2C_Mem_Read(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_READ_OPERATION),
+			MPU_ACCELEROMETER_DATA_REG_START
+					+ MPU_ACCELEROMETER_Y_DATA_REG_OFFSET, 1, l_Data, 2,
+			MPU_MAX_TIMEOUT_MS);
+	uint16_t retData = (l_Data[0] << 8) + l_Data[1];
 
+#if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
+	if (retState == HAL_OK) {
+		printf("Y Data retrieved successfully\n");
+	} else {
+		printf("Y Data retrieving Failed\n");
+	}
+#else
+#endif
+	return retData;
 }
 
 uint16_t MPU_6050_getAccZ() {
+	uint8_t l_Data[2];
+	HAL_StatusTypeDef retState = HAL_I2C_Mem_Read(&hi2c1,
+			((MPU_I2C_ADDRESS << 1) + MPU_I2C_READ_OPERATION),
+			MPU_ACCELEROMETER_DATA_REG_START
+					+ MPU_ACCELEROMETER_Z_DATA_REG_OFFSET, 1, l_Data, 2,
+			MPU_MAX_TIMEOUT_MS);
+	uint16_t retData = (l_Data[0] << 8) + l_Data[1];
 
+#if MPU_DEBUG_STATE == MPU_DEBUG_ENABLED
+	if (retState == HAL_OK) {
+		printf("Z Data retrieved successfully\n");
+	} else {
+		printf("Z Data retrieving Failed\n");
+	}
+#else
+#endif
+	return retData;
 }
 
